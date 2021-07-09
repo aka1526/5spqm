1ef0d2091f1ae7a63ce793d7a43d1b517adf8ec6
Akachai <akachai1526@hotmail.com>
2021-07-09 13:43:50 +0700

    first commit

---
+++ resources/views/pages/page_sidemenu.blade.php
@@ -0,0 +1,31 @@
+<nav class="page-sidebar" id="sidebar">
+    <div id="sidebar-collapse">
+        <div class="admin-block d-flex">
+            <div>
+                <img src="./assets/img/admin-avatar.png" width="45px" />
+            </div>
+            <div class="admin-info">
+                <div class="font-strong">James Brown</div><small>Administrator</small></div>
+        </div>
+        <ul class="side-menu metismenu">
+            <li>
+                <a class="active" href="index.html"><i class="sidebar-item-icon fa fa-th-large"></i>
+                    <span class="nav-label">Dashboard</span>
+                </a>
+            </li>
+            <li class="heading">ตั้งค่าระบบ</li>
+            <li>
+                <a href="javascript:;"><i class="sidebar-item-icon fa fa-bookmark"></i>
+                    <span class="nav-label">กำหนดแผน</span><i class="fa fa-angle-left arrow"></i></a>
+                <ul class="nav-2-level collapse">
+                    <li>
+                        <a href="colors.html">แผนการตรวจ</a>
+                    </li>
+
+
+                </ul>
+            </li>
+
+        </ul>
+    </div>
+</nav>
