f4fce96f2bad69f21da61e9704f57a575dc82100
Akachai <akachai1526@hotmail.com>
2021-07-15 16:53:59 +0700

    first commit

--- resources/views/pages/check_yearmonth.blade.php
+++ resources/views/pages/check_yearmonth.blade.php
@@ -13,14 +13,17 @@
 
                  <div class="ibox-title "> แผนการตรวจพื้นที่ ประจำปี
                    <div class="btn-group ml-3">
-                                  <button class="btn btn-info">2021</button>
+                                  <button class="btn btn-info" id="docyear" name="docyear" value="{{ date('Y')}}">{{ date('Y')}}</button>
                                   <button class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-angle-down"></i></button>
                                   <ul class="dropdown-menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(56px, 33px, 0px); top: 0px; left: 0px; will-change: transform;">
+                                    <li>
+                                        <a class="dropdown-item" href="javascript:;" onclick="changeyear('{{ date('Y')-1}}');" >{{ date('Y')-1}}</a>
+                                    </li>
                                       <li>
-                                          <a class="dropdown-item" href="javascript:;">2021</a>
+                                          <a class="dropdown-item" href="javascript:;" onclick="changeyear('{{ date('Y')}}');" >{{ date('Y')}}</a>
                                       </li>
                                       <li>
-                                          <a class="dropdown-item" href="javascript:;">2022</a>
+                                          <a class="dropdown-item" href="javascript:;" onclick="changeyear('{{ date('Y')+1}}');" >{{ date('Y')+1}}</a>
                                       </li>
 
                                   </ul>
@@ -108,4 +111,10 @@ border-color: #7f8c8d;
 
 @section('jsfooter')
     <!-- <script src="./assets/js/scripts/dashboard_1_demo.js" type="text/javascript"></script> -->
+    <script type="text/javascript">
+      function changeyear(y){
+          $("#docyear").val(y);
+          $("#docyear").text(y);
+      }
+    </script>
 @endsection
