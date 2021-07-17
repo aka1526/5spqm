f424be706676aeffa5503926506a6e2b3501c284
Akachai <akachai1526@hotmail.com>
2021-07-17 13:46:53 +0700

    first commit

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -322,11 +322,60 @@ if($datatype==1){
 }
 
 if($datatype==2){
-    foreach ($QuestionsResult as $key => $row) {
-      $html .='
+      $html .=' <div class="row">';
+        foreach ($QuestionsResult as $key => $row) {
+          $html .='
+          <div class="row">
 
-      ';
-    }
+                         <div class="col-md-6">
+                             <div class="ibox">
+                                 <div class="ibox-head">
+                                     <div class="ibox-title">Horizontal Form</div>
+                                     <div class="ibox-tools">
+                                         <a class="ibox-collapse"><i class="fa fa-minus"></i></a>
+                                         <a class="fullscreen-link"><i class="fa fa-expand"></i></a>
+                                     </div>
+                                 </div>
+                                 <div class="ibox-body">
+                                     <form class="form-horizontal">
+                                         <div class="form-group row">
+                                             <label class="col-sm-2 col-form-label">Email</label>
+                                             <div class="col-sm-10">
+                                                 <input class="form-control" type="text" placeholder="Email address">
+                                             </div>
+                                         </div>
+                                         <div class="form-group row">
+                                             <label class="col-sm-2 col-form-label">Password</label>
+                                             <div class="col-sm-10">
+                                                 <input class="form-control" type="password" placeholder="Password">
+                                             </div>
+                                         </div>
+                                         <div class="form-group row">
+                                             <label class="col-sm-2 col-form-label">Gender</label>
+                                             <div class="col-sm-10">
+                                                 <input class="form-control" type="password" placeholder="Password">
+                                             </div>
+                                         </div>
+                                         <div class="form-group row">
+                                             <div class="col-sm-10 ml-sm-auto">
+                                                 <label class="ui-checkbox ui-checkbox-gray">
+                                                     <input type="checkbox">
+                                                     <span class="input-span"></span>Remamber me</label>
+                                             </div>
+                                         </div>
+                                         <div class="form-group row">
+                                             <div class="col-sm-10 ml-sm-auto">
+                                                 <button class="btn btn-info" type="submit">Submit</button>
+                                             </div>
+                                         </div>
+                                     </form>
+                                 </div>
+                             </div>
+                         </div>
+
+          ';
+        }
+    $html .=' </div>';
 }
 
 
