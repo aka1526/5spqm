6fc43b1b33d6c0b1e53c80c2b5de87de2bfe6de5
Akachai <akachai1526@hotmail.com>
2021-07-17 14:11:17 +0700

    first commit

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -333,50 +333,77 @@ if($datatype==2){
       $html .=' ';
         foreach ($QuestionsResult as $key => $row) {
           $html .='
-<div class="row">
-           <div class="col-md-6">
-                             <div class="ibox">
-                                 <div class="ibox-head">
-                                     <div class="ibox-title">Horizontal Form</div>
-
-                                 </div>
-                                 <div class="ibox-body">
-                                     <form class="form-horizontal">
-                                         <div class="form-group row">
-                                             <label class="col-sm-2 col-form-label">Email</label>
-                                             <div class="col-sm-10">
-                                                 <input class="form-control" type="text" placeholder="Email address">
-                                             </div>
-                                         </div>
-                                         <div class="form-group row">
-                                             <label class="col-sm-2 col-form-label">Password</label>
-                                             <div class="col-sm-10">
-                                                 <input class="form-control" type="password" placeholder="Password">
-                                             </div>
-                                         </div>
-                                         <div class="form-group row">
-                                             <label class="col-sm-2 col-form-label">Gender</label>
-                                             <div class="col-sm-10">
-                                                 <input class="form-control" type="password" placeholder="Password">
-                                             </div>
-                                         </div>
-                                         <div class="form-group row">
-                                             <div class="col-sm-10 ml-sm-auto">
-                                                 <label class="ui-checkbox ui-checkbox-gray">
-                                                     <input type="checkbox">
-                                                     <span class="input-span"></span>Remamber me</label>
-                                             </div>
-                                         </div>
-                                         <div class="form-group row">
-                                             <div class="col-sm-10 ml-sm-auto">
-                                                 <button class="btn btn-info" type="submit">Submit</button>
-                                             </div>
-                                         </div>
-                                     </form>
-                                 </div>
-                             </div>
-                         </div>
- </div>
+          <div class="ibox">
+                  <div class="ibox-head">
+                      <div class="ibox-title">Responsive Table</div>
+                  </div>
+                  <div class="ibox-body">
+                      <div class="table-responsive">
+                          <table class="table">
+                              <thead>
+                                  <tr>
+                                      <th width="50px"></th>
+                                      <th>Product</th>
+                                      <th>Price</th>
+                                      <th>Data</th>
+                                      <th>Last Name</th>
+                                      <th>Actions</th>
+                                  </tr>
+                              </thead>
+                              <tbody>
+                                  <tr>
+                                      <td>
+                                          <label class="ui-checkbox">
+                                              <input type="checkbox">
+                                              <span class="input-span"></span>
+                                          </label>
+                                      </td>
+                                      <td>iphone case</td>
+                                      <td>$1200</td>
+                                      <td>33%</td>
+                                      <td>02/08/2017</td>
+                                      <td>
+                                          <button class="btn btn-default btn-xs m-r-5" data-toggle="tooltip" data-original-title="Edit"><i class="fa fa-pencil font-14"></i></button>
+                                          <button class="btn btn-default btn-xs" data-toggle="tooltip" data-original-title="Delete"><i class="fa fa-trash font-14"></i></button>
+                                      </td>
+                                  </tr>
+                                  <tr>
+                                      <td>
+                                          <label class="ui-checkbox">
+                                              <input type="checkbox">
+                                              <span class="input-span"></span>
+                                          </label>
+                                      </td>
+                                      <td>Car covers</td>
+                                      <td>$3280</td>
+                                      <td>42%</td>
+                                      <td>08/10/2017</td>
+                                      <td>
+                                          <button class="btn btn-default btn-xs m-r-5" data-toggle="tooltip" data-original-title="Edit"><i class="fa fa-pencil font-14"></i></button>
+                                          <button class="btn btn-default btn-xs" data-toggle="tooltip" data-original-title="Delete"><i class="fa fa-trash font-14"></i></button>
+                                      </td>
+                                  </tr>
+                                  <tr>
+                                      <td>
+                                          <label class="ui-checkbox">
+                                              <input type="checkbox">
+                                              <span class="input-span"></span>
+                                          </label>
+                                      </td>
+                                      <td>Compressors</td>
+                                      <td>$7400</td>
+                                      <td>56%</td>
+                                      <td>14/11/2017</td>
+                                      <td>
+                                          <button class="btn btn-default btn-xs m-r-5" data-toggle="tooltip" data-original-title="Edit"><i class="fa fa-pencil font-14"></i></button>
+                                          <button class="btn btn-default btn-xs" data-toggle="tooltip" data-original-title="Delete"><i class="fa fa-trash font-14"></i></button>
+                                      </td>
+                                  </tr>
+                              </tbody>
+                          </table>
+                      </div>
+                  </div>
+              </div>
           ';
         }
     $html .='';
