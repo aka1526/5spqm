b876134ca75e30dd041242dd17536aa713b1dd46
Akachai <akachai1526@hotmail.com>
2021-07-20 10:20:26 +0700

    first commit

--- app/Http/Controllers/QuestionsResultController.php
+++ app/Http/Controllers/QuestionsResultController.php
@@ -139,7 +139,7 @@ class QuestionsResultController extends Controller
                                <button class="btn btn-warning btn-back '.($RowCurrent <=1 ? 'disabled' : '') .'"  data-ans="'.$row->unid_ans.'" data-current="'. $RowCurrent .'" data-back="'.$RowBack.'" onclick="getResult(\''.$row->unid_ans.'\',\''.$RowBack.'\')"> <i class="fa fa-step-backward"></i>กลับ</button>
                                 <button class="btn btn-info" >'.$row->result_index.' / '.count($QuestionsResult).'  </button>';
                                 if($row->result_index ==count($QuestionsResult)) {
-                                    $html .=' <button class="btn btn-primary btn-next"  data-ans="'.$row->unid_ans.'" data-current="'. $RowCurrent .'" data-next="'.$RowNext.'" onclick="getResult(\''.$row->unid_ans.'\',\''.$RowNext.'\')"> ส่งคะแนน <i class="fa fa-step-forward"></i></button>';
+                                    $html .=' <button class="btn btn-primary btn-next"  data-ans="'.$row->unid_ans.'" data-current="'. $RowCurrent .'" data-next="'.$RowNext.'" onclick="getResult(\''.$row->unid_ans.'\',\''.$RowNext.'\')"> ส่งคะแนน <i class="fa fa-award"></i></button>';
                                 } else {
                                       $html .=' <button class="btn btn-primary btn-next"  data-ans="'.$row->unid_ans.'" data-current="'. $RowCurrent .'" data-next="'.$RowNext.'" onclick="getResult(\''.$row->unid_ans.'\',\''.$RowNext.'\')"> ถัดไป <i class="fa fa-step-forward"></i></button>';
                                 }
