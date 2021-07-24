40122598c5e88717f6047eb6f5821eba03962e9b
Akachai <akachai1526@hotmail.com>
2021-07-24 13:10:17 +0700

    first commit

--- resources/views/pages/page_sidemenu.blade.php
+++ resources/views/pages/page_sidemenu.blade.php
@@ -8,29 +8,23 @@
                       <div class="font-strong">Admin</div><small>Administrator</small></div>
               </div>
               <ul class="side-menu metismenu">
-                  <!-- <li>
-                      <a class="active" href="{{ route('dashboard.index')}}"><i class="sidebar-item-icon fa fa-th-large"></i>
-                          <span class="nav-label">Dashboard</span>
-                      </a>
-                  </li> -->
+
                   <li>
                       <a class="active" href="{{ route('check.index')}}"><i class="sidebar-item-icon fa fa-th-large"></i>
                           <span class="nav-label">ตรวจประเมินพื้นที่</span>
                       </a>
                   </li>
 
-                  <li>
-                      <a class="active" href="{{ route('plan.index')}}"><i class="sidebar-item-icon fa fa-calendar"></i>
-                          <span class="nav-label">แผนตรวจประเมิน</span>
-                      </a>
-                  </li>
+
 
                   <li class="heading">ตั้งค่าระบบ</li>
                   <li>
                       <a href="javascript:;"><i class="sidebar-item-icon fa fa-bookmark"></i>
                           <span class="nav-label">ข้อมูลพื้นฐาน</span><i class="fa fa-angle-left arrow"></i></a>
                       <ul class="nav-2-level collapse">
-
+                        <li>
+                            <a  href="{{ route('plan.index')}}">สร้างแผนตรวจประเมิน</a>
+                        </li>
                         <li>
                             <a href="{{ route('questions.index')}}">แบบฟอร์มการตรวจพื้นที่</a>
 
