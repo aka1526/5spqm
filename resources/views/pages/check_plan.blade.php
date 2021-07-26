afe7436f14ef0503b3e78df78b1e8d0d1145a349
Akachai <akachai1526@hotmail.com>
2021-07-26 15:47:29 +0700

    first commit

--- resources/views/pages/check_plan.blade.php
+++ resources/views/pages/check_plan.blade.php
@@ -62,7 +62,7 @@
                                               <input type="hidden" id="area_unid" name="area_unid" value="{{ $row->plan_area_unid }}">
                                               <input type="hidden" id="plan_unid" name="plan_unid" value="{{ $row->unid }}">
 
-                                          <button type=submit class="btn btn btn-primary   m-r-5  " data-unid="{{ $row->unid }}" data-toggle="tooltip" data-original-title="ตรวจประเมินพื้นที่" >
+                                          <button type=submit class="btn btn btn-primary   m-r-5  " style="cursor: pointer;" data-unid="{{ $row->unid }}" data-toggle="tooltip" data-original-title="ตรวจประเมินพื้นที่" >
 
                                           <i class="fas fa-balance-scale font-14 btn-check"></i> ตรวจประเมินพื้นที่ </button>
                                           </form>
@@ -72,7 +72,7 @@
                                               <input type="hidden" id="area_unid" name="area_unid" value="{{ $row->plan_area_unid }}">
                                               <input type="hidden" id="plan_unid" name="plan_unid" value="{{ $row->unid }}">
 
-                                          <button type=submit class="btn btn btn-warning   m-r-5  " data-unid="{{ $row->unid }}" data-toggle="tooltip" data-original-title="คะแนนตรวจประเมิน" >
+                                          <button type=submit class="btn btn btn-warning   m-r-5  " style="cursor: pointer;" data-unid="{{ $row->unid }}" data-toggle="tooltip" data-original-title="คะแนนตรวจประเมิน" >
 
                                             <i class="fas fa-thumbs-up"></i> คะแนนตรวจประเมิน</button>
                                           </form>
