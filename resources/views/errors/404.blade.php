fb1bdf1a18104943d5ffaa81f33fb43f93aeead6
Akachai <akachai1526@hotmail.com>
2021-07-29 16:30:19 +0700

    first commit

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,34 @@
+
+@section('title')
+{{ config('app.name') }}
+@endsection
+
+@section('content-wrapper')
+<div class="content-wrapper">
+       <div class="page-content fade-in-up">
+
+
+           <div class="row">
+             <div class="col-xl-12">
+               <div class="container">
+                  <div class="center" style="margin-top: 150px; margin-bottom: 30px; text-align: center">
+                      <h1>Page not found!</h1>
+                      <p>Sorry, but the page you were looking for could not be found.</p>
+                  </div>
+              </div>
+
+             </div>
+           </div>
+
+       </div>
+       <!-- END PAGE CONTENT-->
+
+   </div>
+
+
+
+@endsection
+
+@section('jsfooter')
+
+@endsection
